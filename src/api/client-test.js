cf794854323c7975097a0cc49152c61b18138964
hangnguyenn <thuyhanghcmus@gmail.com>
2021-08-15 22:58:57 +0700

    Update api LIST_CLIENT_TEST_BY_ADMIN

--- src/api/client-test.js
+++ src/api/client-test.js
@@ -4,3 +4,8 @@ export const LIST_CLIENT_TEST = () => ({
   method: METHOD.GET,
   url: "/api/v1/client-tests/",
 });
+
+export const LIST_CLIENT_TEST_BY_ADMIN = () => ({
+  method: METHOD.GET,
+  url: "/api/v1/client-tests/by-admin",
+})
