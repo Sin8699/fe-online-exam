93b5b28db3b5385eb45f676003a4b6b40945bf70
thanhdinh <dinhcongthanh1910@gmail.com>
2021-08-16 00:32:54 +0700

    get profile user

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,45 +1,45 @@
-import { Box, Container, Grid } from "@material-ui/core";
-import AccountProfile from "../components/account/AccountProfile";
-import { useState,useEffect } from "react";
-import AccountProfileDetails from "../components/account/AccountProfileDetails";
-import ChangePassword from "../components/account/ChangePassword";
-import Page from "../components/Page";
-import { GET_INFO_PROFILE_CLIENT, GET_INFO_PROFILE_MANAGER, UPDATE_INFO_PROFILE_CLIENT, UPDATE_INFO_PROFILE_MANAGER } from "../api/auth";
-import checkRole from "../helpers/checkRole";
+import { Box, Container, Grid } from '@material-ui/core';
+import AccountProfile from '../components/account/AccountProfile';
+import { useState, useEffect } from 'react';
+import AccountProfileDetails from '../components/account/AccountProfileDetails';
+import ChangePassword from '../components/account/ChangePassword';
+import Page from '../components/Page';
+import { GET_INFO_PROFILE_CLIENT, GET_INFO_PROFILE_MANAGER, UPDATE_INFO_PROFILE_CLIENT, UPDATE_INFO_PROFILE_MANAGER } from '../api/auth';
+import checkRole from '../helpers/checkRole';
 import useAxios from '../hooks/useAxios';
 
 // ----------------------------------------------------------------------
 
 export default function Profile() {
-  const [userInfo,setUserInfo] = useState({})
-  
-  
   const { isClient } = checkRole();
+  //Cách 1
+  // let func;
+  // isClient ? (func = GET_INFO_PROFILE_CLIENT()) : (func = GET_INFO_PROFILE_MANAGER());
+  // const { response: resClientTest } = useAxios(func);
+
+  //Cách 2 ---------------------------------------------------------------------------------------------------------------------
+  const [userInfo, setUserInfo] = useState({});
   const { response: profile, fetchData: getProfileClient } = useAxios(GET_INFO_PROFILE_CLIENT(), false);
-  const { response: profileManager, fetchData: getProfileManager } = useAxios(GET_INFO_PROFILE_MANAGER(),false);
- 
-  const getProfileClientFunc = async () =>{
-    const code  = await getProfileClient();
-    console.log('code: ', code);
-  }
-  const getProfileAdmin = async () =>{
-    const code  = await getProfileManager();
-    if (code === 0) {console.log(profileManager)} 
-  }
-
-  useEffect(()=>{
-    isClient ? getProfileClientFunc() : getProfileAdmin()
-  }, [])
-
+  const { response: profileManager, fetchData: getProfileManager } = useAxios(GET_INFO_PROFILE_MANAGER(), false);
+  useEffect(() => {
+    (async function () {
+      if (isClient) await getProfileClient();
+      else await getProfileManager();
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    setUserInfo(isClient ? profile : profileManager);
+  }, [isClient, profile, profileManager]);
   console.log('userInfo: ', userInfo);
-  
+  //---------------------------------------------------------------------------------------------------------------------------
 
   return (
     <Page title="Dashboard | Profile | Online Exam-UI">
       <Box
         sx={{
-          backgroundColor: "background.default",
-          minHeight: "100%",
+          backgroundColor: 'background.default',
+          minHeight: '100%',
           py: 3,
         }}
       >
