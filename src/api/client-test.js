1caa0de4770e5734f16b12a8e2196b5c4feec8bc
Phuong Dao <phuong.dao@inkr.com>
2021-09-17 19:12:17 +0700

    update ui admin

--- src/api/client-test.js
+++ src/api/client-test.js
@@ -1,6 +1,11 @@
-import { METHOD } from './config/method'
+import { METHOD } from "./config/method";
 
 export const CLIENT_TEST = (id) => ({
   method: METHOD.GET,
-  url: `test-kit/question-to-test/${id}`
-})
+  url: `test-kit/question-to-test/${id}`,
+});
+
+export const TEST_LIST = () => ({
+  method: METHOD.GET,
+  url: `/tests/admin`,
+});
